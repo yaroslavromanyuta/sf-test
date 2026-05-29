f9baa013078eeb09eec26ba5613ae497a2ba38c4
Yaroslav <yaroslav.romanyuta@gmail.com>
2026-05-29 22:20:42 +0300

    Update build configuration and adjust library versions
    - Refactor Kotlin JVM target configuration to use modern compiler options DSL
    - Enable AndroidX and Jetifier in `gradle.properties`
    - Adjust Hilt and AndroidX Core KTX versions in the version catalog
    - Reorder Gradle plugins and update imports in the app module build script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.kotlinCompose)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.hilt)
@@ -34,11 +36,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
     }
 }
 
